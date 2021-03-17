3a6861b5cb4c3670f0acafe021a856d9fe142a8a
Jake Molnar <jake.molnar@gmail.com>
2021-03-17 09:46:19 -0600

    test neighbor_counter_clockwise_of

--- test/map_node_test.cpp
+++ test/map_node_test.cpp
@@ -43,3 +43,42 @@ TEST(MapNode, neighbor_clockwise) {
 		EXPECT_EQ(*expected_clockwise_nbor, *actual_clockwise_nbor);
 	}
 }
+
+TEST(MapNode, neighbor_counter_clockwise) {
+	// Node 2 should have three neighbors: 192, 193, and 194
+	// which are in clockwise order. Let's make sure we can
+	// go counter-clockwise in-between them.
+	const auto& node_2 = default_map.map_node(wl::map_id(2));
+
+	// now, we don't actually care which is the first one
+	// (we only care that they are in counter-clockwise order),
+	// so set up our expectations with a function:
+	const auto expected_after = [] (wl::MapNode::ID id) -> std::optional<wl::MapNode::ID> {
+		constexpr wl::MapNode::ID k_192{wl::map_id(192u)};
+		constexpr wl::MapNode::ID k_193{wl::map_id(193u)};
+		constexpr wl::MapNode::ID k_194{wl::map_id(194u)};
+		if (id == k_192) {
+			return k_194;
+		} else if (id == k_193) {
+			return k_192;
+		} else if (id == k_194) {
+			return k_193;
+		}
+		return std::nullopt;
+	};
+
+	EXPECT_EQ(3u, node_2.neighbors().size());
+	for (const wl::MapNode::ID nbor_id : node_2.neighbors()) {
+		const auto expected_counter_clockwise_nbor = expected_after(nbor_id);
+		if (!expected_counter_clockwise_nbor.has_value()) {
+			FAIL() << "got unexpected neighbor " << static_cast<std::size_t>(nbor_id);
+			continue;
+		}
+		const auto actual_counter_clockwise_nbor = node_2.neighbor_counter_clockwise_of(nbor_id);
+		if (!actual_counter_clockwise_nbor.has_value()) {
+			FAIL() << "neighbor " << static_cast<std::size_t>(nbor_id) << " had no counter-clockwise neighbor";
+			continue;
+		}
+		EXPECT_EQ(*expected_counter_clockwise_nbor, *actual_counter_clockwise_nbor);
+	}
+}
