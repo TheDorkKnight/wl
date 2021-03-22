a446698ed72946572f6306b4cd6206a686649a9f
TheDorkKnight <jake.molnar@gmail.com>
2021-03-22 10:22:30 -0600

    fix missing line after Normal nodes

--- apps/printmap/src/main.cpp
+++ apps/printmap/src/main.cpp
@@ -36,8 +36,9 @@ void print_jack_node(const wl::JackNode& jack_node,
 	                 const wl::MapNode& map_node) {
 	std::cout << "\t\tneighbors: ";
 	print_adjacency_list(map_node.neighbors());
+	std::cout << std::endl;
 	if (!jack_node.can_drop_evidence()) {
-		std::cout << std::endl << "\t\tcan drop evidence: false" << std::endl;
+		std::cout << "\t\tcan drop evidence: false" << std::endl;
 	}
 	if (jack_node.is_water()) {
 		std::cout << "\t\tis water: true" << std::endl;
