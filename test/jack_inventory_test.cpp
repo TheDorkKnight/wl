c6d8fd0bd925eec96ba8497ffeb489b39c777764
Jake Molnar <jake.molnar@gmail.com>
2021-04-21 21:51:49 -0600

    test that decrementing inventory works

--- test/jack_inventory_test.cpp
+++ test/jack_inventory_test.cpp
@@ -7,3 +7,28 @@ TEST(JackInventory, default_values) {
 	EXPECT_EQ(2u, inventory.num_alleys());
 	EXPECT_EQ(2u, inventory.num_boats());
 }
+
+TEST(JackInventory, can_decrement_values) {
+	wl::JackInventory inventory;
+	{
+		const auto original_num_carriages = inventory.num_carriages();
+		ASSERT_NE(0u, original_num_carriages);
+
+		inventory.use_carriage();
+		EXPECT_EQ(original_num_carriages - 1u, inventory.num_carriages());
+	}
+	{
+		const auto original_num_alleys = inventory.num_alleys();
+		ASSERT_NE(0u, original_num_alleys);
+
+		inventory.use_alley();
+		EXPECT_EQ(original_num_alleys - 1u, inventory.num_alleys());
+	}
+	{
+		const auto original_num_boats = inventory.num_boats();
+		ASSERT_NE(0u, original_num_boats);
+
+		inventory.use_boat();
+		EXPECT_EQ(original_num_boats - 1u, inventory.num_boats());
+	}
+}
