ab7d69b5af713dd19068dda89dadba2756a3ca0f
Rafuzee <contactrigel@gmail.com>
2021-05-06 01:21:20 -0400

    Define jack move no test

---
+++ include/wl/j_moves.h
@@ -0,0 +1,34 @@
+#ifndef WL_J_MOVES_H
+#define WL_J_MOVES_H
+
+#include <cstdint>
+#include <cassert>
+#include <array>
+#include <optional>
+#include <wl/wl.h>
+#include <wl/jack_inventory.h>
+
+namespace wl {
+
+class JackMove {
+	MapNode::ID where_to_;
+	std::optional<JackResource> resource_;
+
+public:
+	constexpr JackMove(MapNode::ID whereto) noexcept
+		: where_to_{ whereto }
+		, resource_{ std::nullopt }
+	{}
+	constexpr JackMove(MapNode::ID whereto, JackResource move_cost) noexcept
+		: where_to_{ whereto }
+		, resource_{ move_cost }
+	{}
+
+	constexpr MapNode::ID destination() const noexcept { return where_to_; }
+	constexpr std::optional<JackResource> cost() const noexcept { return resource_; }
+};
+
+} // namespace
+
+#endif
+
