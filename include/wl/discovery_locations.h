564e080efd48f56df9c9ab66d4f1adfacfe129bc
Jake Molnar <jake.molnar@gmail.com>
2021-05-27 10:15:43 -0600

    add quick tracker for discovery locations

---
+++ include/wl/discovery_locations.h
@@ -0,0 +1,31 @@
+#ifndef WL_DISCOVERY_LOCATIONS_H
+#define WL_DISCOVERY_LOCATIONS_H
+
+#include <cstdint>
+#include <cassert>
+#include <array>
+#include <wl/wl.h>
+
+namespace wl {
+
+class DiscoveryLocations {
+	std::array<MapNode::ID,4> locations_;
+
+public:
+	using iterator = std::array<MapNode::ID,4>::const_iterator;
+
+	constexpr DiscoveryLocations(MapNode::ID loc0,
+		                         MapNode::ID loc1,
+		                         MapNode::ID loc2,
+		                         MapNode::ID loc3) noexcept
+		: locations_{loc0, loc1, loc2, loc3}
+	{}
+
+	constexpr iterator begin() const noexcept { return locations_.begin(); }
+	constexpr iterator end() const noexcept { return locations_.end(); }
+	constexpr auto size() const noexcept { return locations_.size(); }
+};
+
+} // namespace
+
+#endif
